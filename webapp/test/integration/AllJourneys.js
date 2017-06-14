0ea09957358e34e1eb442264c0f2f35c76d821b1
Matthias Osswald <mat.osswald@sap.com>
2017-06-14 14:21:06 +0200

    Add tests for filter tabs

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -2,7 +2,8 @@ sap.ui.define([
 	"sap/ui/test/Opa5",
 	"sap/ui/demo/todo/test/integration/pages/Common",
 	"sap/ui/demo/todo/test/integration/TodoListJourney",
-	"sap/ui/demo/todo/test/integration/SearchJourney"
+	"sap/ui/demo/todo/test/integration/SearchJourney",
+	"sap/ui/demo/todo/test/integration/FilterJourney"
 ], function(Opa5, Common) {
 	"use strict";
 
