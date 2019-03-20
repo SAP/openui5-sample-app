a31593f778941a7342ad622ef9b341b93f4052e3
Matthias Osswald <mat.osswald@sap.com>
2019-03-20 16:00:45 +0100

    Configure UI5 URL to use CDN instead local npm dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,10 @@ module.exports = function(config) {
 
 		frameworks: ["ui5"],
 
+		ui5: {
+			url: "https://openui5.hana.ondemand.com"
+		},
+
 		browsers: ["Chrome"],
 
 		browserConsoleLogOptions: {
