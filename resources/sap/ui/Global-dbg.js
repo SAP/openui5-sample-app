9725d1cc8064e7ac88a14024f1d7acee9e43c68c
openui5bot <openui5bot@users.noreply.github.com>
2023-08-22 13:36:42 +0000

    deploy: 472cf3f1125a3d380c4ed7fd92dac85036080b94

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -78,7 +78,7 @@ sap.ui.define([
 		 */
 		version: "1.117.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20230821-0722" }
+		buildinfo : { lastchange : "", buildtime : "20230822-1335" }
 	});
 
 	var syncCallBehavior = Configuration.getSyncCallBehavior();
