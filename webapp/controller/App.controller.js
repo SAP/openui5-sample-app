eda58eac12fa3e848f3397694cdc4470bc6efb28
Fabian Tschirschnitz <fabian.tschirchnitz@dsc-gmbh.com>
2023-12-14 11:18:48 +0100

    feat: add date field to todos

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,146 +1,173 @@
-sap.ui.define([
-	"sap/ui/Device",
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	"sap/ui/model/json/JSONModel"
-], (Device, Controller, Filter, FilterOperator, JSONModel) => {
-	"use strict";
+sap.ui.define(
+	[
+		"sap/ui/Device",
+		"sap/ui/core/mvc/Controller",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator",
+		"sap/ui/model/json/JSONModel",
+	],
+	(Device, Controller, Filter, FilterOperator, JSONModel) => {
+		"use strict";
 
-	return Controller.extend("sap.ui.demo.todo.controller.App", {
+		return Controller.extend("sap.ui.demo.todo.controller.App", {
+			onInit() {
+				this.aSearchFilters = [];
+				this.aTabFilters = [];
 
-		onInit() {
-			this.aSearchFilters = [];
-			this.aTabFilters = [];
+				this.getView().setModel(
+					new JSONModel({
+						isMobile: Device.browser.mobile,
+						filterText: undefined,
+					}),
+					"view"
+				);
+			},
 
-			this.getView().setModel(new JSONModel({
-				isMobile: Device.browser.mobile,
-				filterText: undefined
-			}), "view");
-		},
+			/**
+			 * Adds a new todo item to the bottom of the list.
+			 */
+			addTodo() {
+				const oModel = this.getView().getModel();
+				const aTodos = oModel
+					.getProperty("/todos")
+					.map((oTodo) => Object.assign({}, oTodo));
 
-		/**
-		 * Adds a new todo item to the bottom of the list.
-		 */
-		addTodo() {
-			const oModel = this.getView().getModel();
-			const aTodos = oModel.getProperty("/todos").map((oTodo) => Object.assign({}, oTodo));
+				aTodos.push({
+					title: oModel.getProperty("/newTodo/title"),
+					dueDate: oModel.getProperty("/newTodo/dueDate"),
+					completed: false,
+				});
 
-			aTodos.push({
-				title: oModel.getProperty("/newTodo"),
-				completed: false
-			});
+				oModel.setProperty("/todos", aTodos);
+				oModel.setProperty("/newTodo", { title: "", dueDate: null });
+			},
 
-			oModel.setProperty("/todos", aTodos);
-			oModel.setProperty("/newTodo", "");
-		},
+			/**
+			 * Removes all completed items from the todo list.
+			 */
+			clearCompleted() {
+				const oModel = this.getView().getModel();
+				const aTodos = oModel
+					.getProperty("/todos")
+					.map((oTodo) => Object.assign({}, oTodo));
 
-		/**
-		 * Removes all completed items from the todo list.
-		 */
-		clearCompleted() {
-			const oModel = this.getView().getModel();
-			const aTodos = oModel.getProperty("/todos").map((oTodo) => Object.assign({}, oTodo));
-
-			let i = aTodos.length;
-			while (i--) {
-				const oTodo = aTodos[i];
-				if (oTodo.completed) {
-					aTodos.splice(i, 1);
+				let i = aTodos.length;
+				while (i--) {
+					const oTodo = aTodos[i];
+					if (oTodo.completed) {
+						aTodos.splice(i, 1);
+					}
 				}
-			}
 
-			oModel.setProperty("/todos", aTodos);
-		},
+				oModel.setProperty("/todos", aTodos);
+			},
 
-		/**
-		 * Updates the number of items not yet completed
-		 */
-		updateItemsLeftCount() {
-			const oModel = this.getView().getModel();
-			const aTodos = oModel.getProperty("/todos") || [];
+			/**
+			 * Updates the number of items not yet completed
+			 */
+			updateItemsLeftCount() {
+				const oModel = this.getView().getModel();
+				const aTodos = oModel.getProperty("/todos") || [];
 
-			const iItemsLeft = aTodos.filter((oTodo) => oTodo.completed !== true).length;
+				const iItemsLeft = aTodos.filter(
+					(oTodo) => oTodo.completed !== true
+				).length;
 
-			oModel.setProperty("/itemsLeftCount", iItemsLeft);
-		},
+				oModel.setProperty("/itemsLeftCount", iItemsLeft);
+			},
 
-		/**
-		 * Trigger search for specific items. The removal of items is disable as long as the search is used.
-		 * @param {sap.ui.base.Event} oEvent Input changed event
-		 */
-		onSearch(oEvent) {
-			const oModel = this.getView().getModel();
+			/**
+			 * Trigger search for specific items. The removal of items is disable as long as the search is used.
+			 * @param {sap.ui.base.Event} oEvent Input changed event
+			 */
+			onSearch(oEvent) {
+				const oModel = this.getView().getModel();
 
-			// First reset current filters
-			this.aSearchFilters = [];
+				// First reset current filters
+				this.aSearchFilters = [];
 
-			// add filter for search
-			this.sSearchQuery = oEvent.getSource().getValue();
-			if (this.sSearchQuery && this.sSearchQuery.length > 0) {
-				oModel.setProperty("/itemsRemovable", false);
-				const filter = new Filter("title", FilterOperator.Contains, this.sSearchQuery);
-				this.aSearchFilters.push(filter);
-			} else {
-				oModel.setProperty("/itemsRemovable", true);
-			}
-
-			this._applyListFilters();
-		},
-
-		onFilter(oEvent) {
-			// First reset current filters
-			this.aTabFilters = [];
-
-			// add filter for search
-			this.sFilterKey = oEvent.getParameter("item").getKey();
-
-			// eslint-disable-line default-case
-			switch (this.sFilterKey) {
-				case "active":
-					this.aTabFilters.push(new Filter("completed", FilterOperator.EQ, false));
-					break;
-				case "completed":
-					this.aTabFilters.push(new Filter("completed", FilterOperator.EQ, true));
-					break;
-				case "all":
-				default:
-					// Don't use any filter
-			}
-
-			this._applyListFilters();
-		},
-
-		_applyListFilters() {
-			const oList = this.byId("todoList");
-			const oBinding = oList.getBinding("items");
-
-			oBinding.filter(this.aSearchFilters.concat(this.aTabFilters), "todos");
-
-			let sI18nKey;
-			if (this.sFilterKey && this.sFilterKey !== "all") {
-				if (this.sFilterKey === "active") {
-					sI18nKey = "ACTIVE_ITEMS";
+				// add filter for search
+				this.sSearchQuery = oEvent.getSource().getValue();
+				if (this.sSearchQuery && this.sSearchQuery.length > 0) {
+					oModel.setProperty("/itemsRemovable", false);
+					const filter = new Filter(
+						"title",
+						FilterOperator.Contains,
+						this.sSearchQuery
+					);
+					this.aSearchFilters.push(filter);
 				} else {
-					// completed items: sFilterKey = "completed"
-					sI18nKey = "COMPLETED_ITEMS";
+					oModel.setProperty("/itemsRemovable", true);
 				}
-				if (this.sSearchQuery) {
-					sI18nKey += "_CONTAINING";
+
+				this._applyListFilters();
+			},
+
+			onFilter(oEvent) {
+				// First reset current filters
+				this.aTabFilters = [];
+
+				// add filter for search
+				this.sFilterKey = oEvent.getParameter("item").getKey();
+
+				// eslint-disable-line default-case
+				switch (this.sFilterKey) {
+					case "active":
+						this.aTabFilters.push(
+							new Filter("completed", FilterOperator.EQ, false)
+						);
+						break;
+					case "completed":
+						this.aTabFilters.push(
+							new Filter("completed", FilterOperator.EQ, true)
+						);
+						break;
+					case "all":
+					default:
+					// Don't use any filter
 				}
-			} else if (this.sSearchQuery) {
-				sI18nKey = "ITEMS_CONTAINING";
-			}
 
-			let sFilterText;
-			if (sI18nKey) {
-				const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
-				sFilterText = oResourceBundle.getText(sI18nKey, [this.sSearchQuery]);
-			}
+				this._applyListFilters();
+			},
 
-			this.getView().getModel("view").setProperty("/filterText", sFilterText);
-		},
+			_applyListFilters() {
+				const oList = this.byId("todoList");
+				const oBinding = oList.getBinding("items");
 
-	});
+				oBinding.filter(
+					this.aSearchFilters.concat(this.aTabFilters),
+					"todos"
+				);
 
-});
+				let sI18nKey;
+				if (this.sFilterKey && this.sFilterKey !== "all") {
+					if (this.sFilterKey === "active") {
+						sI18nKey = "ACTIVE_ITEMS";
+					} else {
+						// completed items: sFilterKey = "completed"
+						sI18nKey = "COMPLETED_ITEMS";
+					}
+					if (this.sSearchQuery) {
+						sI18nKey += "_CONTAINING";
+					}
+				} else if (this.sSearchQuery) {
+					sI18nKey = "ITEMS_CONTAINING";
+				}
+
+				let sFilterText;
+				if (sI18nKey) {
+					const oResourceBundle = this.getView()
+						.getModel("i18n")
+						.getResourceBundle();
+					sFilterText = oResourceBundle.getText(sI18nKey, [
+						this.sSearchQuery,
+					]);
+				}
+
+				this.getView()
+					.getModel("view")
+					.setProperty("/filterText", sFilterText);
+			},
+		});
+	}
+);
