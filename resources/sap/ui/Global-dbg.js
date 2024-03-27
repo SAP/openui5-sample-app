d8df630a91e9fcda7bc8d632a562a6654b32fce3
openui5bot <openui5bot@users.noreply.github.com>
2024-03-27 12:18:43 +0000

    deploy: 059febd1d6cdac68c822fb98246cb05b0b479ad9

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -71,7 +71,7 @@ sap.ui.define([
 		 */
 		version: "1.122.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20240322-1324" }
+		buildinfo : { lastchange : "", buildtime : "20240327-1218" }
 	};
 
 	/**
