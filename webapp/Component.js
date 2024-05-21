5aed552d425568723a3250f0b69331217177d8d4
Yavor Ivanov <yavor.ivanov@sap.com>
2024-05-21 12:30:09 +0300

    Introduce UI5Lint issues

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,7 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport", "sap/m/DateTimeInput"], (UIComponent, ComponentSupport, DateTimeInput) => {
 	"use strict";
+	const dti = new DateTimeInput();
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
-		metadata: {
-			manifest: "json",
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
-		}
+
 	});
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,8 +4,10 @@ sap.ui.define([
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	"sap/ui/model/json/JSONModel",
-	"sap/base/strings/formatMessage"
-], (Device, Controller, Filter, FilterOperator, JSONModel, formatMessage) => {
+	"sap/base/strings/formatMessage",
+	"sap/ui/core/BarColor",
+	"sap/ui/demo/todo/util/Helper",
+], (Device, Controller, Filter, FilterOperator, JSONModel, formatMessage, BarColor, Helper) => {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.todo.controller.App", {
@@ -13,10 +15,29 @@ sap.ui.define([
 		onInit() {
 			this.aSearchFilters = [];
 			this.aTabFilters = [];
+			this.BarColor = BarColor;
 
 			this.getView().setModel(new JSONModel({
 				isMobile: Device.browser.mobile
 			}), "view");
+		},
+
+		onAfterRendering() {
+			const avatarDOM = jQuery("#container-todo---app--avatar-profile");
+			const avatarCtr = avatarDOM.control(0);
+			avatarCtr.setSrc(Helper.resolvePath('./img/logo_ui5.png'));
+
+			sap.ui.require(["sap/m/Button"], (Button) => {
+				const clearBtn = new Button({
+					id: "clearCompleted",
+					enabled: "{/itemsRemovable}",
+					icon: "sap-icon://delete",
+					text: "{i18n>CLEAR_COMPLETED}",
+					tap: this.clearCompleted.bind(this),
+				});
+
+				this.byId("toolbar").addContent(clearBtn);
+			});
 		},
 
 		/**
@@ -132,7 +153,8 @@ sap.ui.define([
 		},
 
 		_applyListFilters() {
-			const oList = this.byId("todoList");
+			const oList = sap.ui.getCore().byId("container-todo---app--todoList");
+			// const oList = this.byId("todoList");
 			const oBinding = oList.getBinding("items");
 
 			oBinding.filter(this.aSearchFilters.concat(this.aTabFilters), "todos");
