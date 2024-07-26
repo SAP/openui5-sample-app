8139094b41d54c3ba285943b89471cba8b4f7bcd
maxreichmann <maxreichmann@users.noreply.github.com>
2024-07-26 10:22:12 +0000

    deploy: 1816c0cf86f64cb4027e7b022048a84020a09c70

--- Component.js
+++ Component.js
@@ -1,2 +1,2 @@
-sap.ui.define(["sap/ui/core/UIComponent","sap/ui/core/ComponentSupport"],e=>{"use strict";return e.extend("sap.ui.demo.todo.Component",{metadata:{manifest:"json"}})});
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/core/ComponentSupport"],e=>{"use strict";return e.extend("sap.ui.demo.todo.Component",{metadata:{manifest:"json",interfaces:["sap.ui.core.IAsyncContentCreation"]}})});
 //# sourceMappingURL=Component.js.map
