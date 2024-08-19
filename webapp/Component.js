8c0486f868e6d546f288c75e4fc18f323036cbaa
Merlin Beutlberger <m.beutlberger@sap.com>
2024-08-19 15:39:44 +0200

    Introduce ui5lint warning

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,6 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport", "sap/m/DateTimeInput"], (UIComponent, ComponentSupport, DateTimeInput) => {
 	"use strict";
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
-		metadata: {
-			manifest: "json",
-			interfaces: ["sap.ui.core.IAsyncContentCreation"],
-		}
+
 	});
 });
