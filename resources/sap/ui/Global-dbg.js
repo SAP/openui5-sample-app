37f9dbb27a97eff6e5bd90249d418dfb9d556f18
flovogt <flovogt@users.noreply.github.com>
2024-10-18 07:36:15 +0000

    deploy: 8ff64b428886c498e6688b14fe09b096854ec71a

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -61,7 +61,7 @@ sap.ui.define([
 		 */
 		version: "1.129.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20241015-0702" }
+		buildinfo : { lastchange : "", buildtime : "20241018-0735" }
 	};
 
 	/**
