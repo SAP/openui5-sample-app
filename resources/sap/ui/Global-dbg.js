5a736ba02e920b1f8f03f5df1a2dbad5faa7bcf4
matz3 <matz3@users.noreply.github.com>
2025-02-17 16:20:52 +0000

    deploy: 5d73a3177b5d223a94a689957580d4396b1200cb

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -61,7 +61,7 @@ sap.ui.define([
 		 */
 		version: "1.132.1",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20250216-0325" }
+		buildinfo : { lastchange : "", buildtime : "20250217-1620" }
 	};
 
 	/**
