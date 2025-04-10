1b398a54050000df381754ebfbbeb8d10207ce4e
flovogt <flovogt@users.noreply.github.com>
2025-04-10 08:25:08 +0000

    deploy: 74f60c7576bceb2c231b48a4fd1522d85efcff33

--- resources/jquery-sap.js
+++ resources/jquery-sap.js
@@ -108,7 +108,6 @@ sap.ui.predefine("jquery.sap.strings", ["jquery.sap.global","sap/base/strings/ca
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 sap.ui.predefine("jquery.sap.xml", ["jquery.sap.global","sap/ui/util/XMLHelper"],function(jQuery,e){"use strict";jQuery.sap.parseXML=e.parse;jQuery.sap.serializeXML=function(r){var a="";if(window.ActiveXObject){a=r.xml;if(a){return a}}if(window.XMLSerializer){return e.serialize(r)}return a};jQuery.sap.isEqualNode=function(e,r){if(e===r){return true}if(!e||!r){return false}if(e.isEqualNode){return e.isEqualNode(r)}if(e.nodeType!=r.nodeType){return false}if(e.nodeValue!=r.nodeValue){return false}if(e.baseName!=r.baseName){return false}if(e.nodeName!=r.nodeName){return false}if(e.nameSpaceURI!=r.nameSpaceURI){return false}if(e.prefix!=r.prefix){return false}if(e.nodeType!=1){return true}if(e.attributes.length!=r.attributes.length){return false}for(var a=0;a<e.attributes.length;a++){if(!jQuery.sap.isEqualNode(e.attributes[a],r.attributes[a])){return false}}if(e.childNodes.length!=r.childNodes.length){return false}for(var a=0;a<e.childNodes.length;a++){if(!jQuery.sap.isEqualNode(e.childNodes[a],r.childNodes[a])){return false}}return true};jQuery.sap.getParseError=e.getParseError;return jQuery});
-sap.ui.requireSync("jquery.sap.encoder");
 sap.ui.requireSync("jquery.sap.xml");
 sap.ui.requireSync("jquery.sap.strings");
 sap.ui.requireSync("jquery.sap.sjax");
@@ -119,5 +118,6 @@ sap.ui.requireSync("jquery.sap.mobile");
 sap.ui.requireSync("jquery.sap.keycodes");
 sap.ui.requireSync("jquery.sap.global");
 sap.ui.requireSync("jquery.sap.events");
+sap.ui.requireSync("jquery.sap.encoder");
 sap.ui.requireSync("jquery.sap.dom");
 //# sourceMappingURL=jquery-sap.js.map
