8b42fe849a2e20e9b37194b077ea5a96edc1131e
Matthias Osswald <mat.osswald@sap.com>
2025-05-23 13:03:26 +0200

    Initial app state

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,10 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport", "sap/m/DateTimeInput"], (UIComponent, ComponentSupport, DateTimeInput) => {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], (UIComponent) => {
 	"use strict";
-	const dti = new DateTimeInput();
-	return UIComponent.extend("sap.ui.demo.todo.Component", {
 
+	return UIComponent.extend("sap.ui.demo.todo.Component", {
+		metadata: {
+			manifest: "json",
+			interfaces: ["sap.ui.core.IAsyncContentCreation"]
+		}
 	});
 });
